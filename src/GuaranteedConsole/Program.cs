b0f4bc9f56aa4191446bc5392347c36873775a3a
Skipper <erikcheatham@gmail.com>
2016-11-18 17:38:41 -0600

    Sorting And Output Finished

--- src/GuaranteedConsole/Program.cs
+++ src/GuaranteedConsole/Program.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using GuaranteedConsole.Entities;
 using System.IO;
 using System.Text;
+using System.ComponentModel;
 
 namespace GuaranteedConsole
 {
@@ -25,7 +26,14 @@ namespace GuaranteedConsole
             List<Person> CommaList = ParseFiles(commas, ',');
             List<Person> SpaceList = ParseFiles(spaces, ' ');
 
+            PipeList.SortByGender();
+            CommaList.SortByBirthDate();
+            SpaceList.SortByLastName();
 
+            ShowOutput(PipeList, "Output 1 - Sorted By Gender");
+            ShowOutput(CommaList, "Output 2 - Sorted By Birth Date Ascending");
+            ShowOutput(SpaceList, "Output 3 - Sorted By Last Name Descending");
+            Console.ReadLine();
         }
 
         public static List<Person> ParseFiles(string[] file, char type)
@@ -96,6 +104,60 @@ namespace GuaranteedConsole
                     return null;
             }
             return List;
+        }
+
+
+        public static void ShowOutput(List<Person> person, string type)
+        {
+            Console.WriteLine();
+            Console.WriteLine(type);
+            foreach (Person p in person)
+            {
+                ////Print Headers
+                //foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(p))
+                //{
+                //    string name = descriptor.Name;
+                //    Console.Write(name);
+                //}
+                ////Print Line Break
+                //Console.WriteLine();
+                foreach (PropertyDescriptor descriptor in TypeDescriptor.GetProperties(p))
+                {
+                    object value = descriptor.GetValue(p);
+                    Console.Write(value + "     ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static List<Person> SortByGender(this List<Person> p)
+        {
+            if (p.Any())
+            {
+                p.Sort((p1, p2) => p1.Gender.CompareTo(p2.Gender));
+            }
+            return p;
+        }
+
+        public static List<Person> SortByBirthDate(this List<Person> p)
+        {
+            if (p.Any())
+            {
+                p.Sort((p1, p2) => p1.DateOfBirth.CompareTo(p2.DateOfBirth));
+            }
+            return p;
+        }
+
+        public static List<Person> SortByLastName(this List<Person> p)
+        {
+            if (p.Any())
+            {
+                p.Sort((p1, p2) => -1 * p1.LastName.CompareTo(p2.LastName));
+            }
+            return p;
         }
     }
 }
