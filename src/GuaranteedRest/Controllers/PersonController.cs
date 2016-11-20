f2f1727797817076d6cf9b3bf97cbe5996997071
Skipper <erikcheatham@gmail.com>
2016-11-20 17:35:39 -0600

    Added Nuget CLI & Created Package (Core Requires Core Projects Be Installed Via Nuget, So I Can Install The Console Program Into The Rest API)

---
+++ src/GuaranteedRest/Controllers/PersonController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GuaranteedRest.Controllers
+{
+    [Route("api/person")]
+    public class PersonController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            //return RunProgram.Run("PipeDelimited", "gender", "asc", '|');
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        //// POST api/values
+        //[HttpPost]
+        //public void Post([FromBody]string value)
+        //{
+        //}
+
+        //// PUT api/values/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+
+        //// DELETE api/values/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ src/GuaranteedRest/Models/PersonAPI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuaranteedRest.Models
+{
+    public class PersonAPI
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string FavoriteColor { get; set; }
+        public string DateOfBirth { get; set; }
+    }
+}
